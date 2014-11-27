6a70eb7ffcbe158db8629a9f3aa5a284b88bce01
filiptuhy <filip.tuhy@gmail.com>
2014-11-28 01:50:09 +0200

    Added all files

---
+++ src/src/Files.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package src;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author filipovsluha
+ */
+public class Files {
+    private HashMap<String, String> filezzHashMap = new HashMap<String, String>();
+    List<String> duplicatezzList = new ArrayList<>();
+    private int mp3Number = 1;
+
+    public void add(String path, String fileName) {
+        filezzHashMap.put(path,fileName);
+    }
+    
+    public int addAllFromFolder(File folder)
+    {
+         File[] listOfFiles = folder.listFiles();
+        if(listOfFiles != null){
+            for (File oneFile : listOfFiles) {
+                if (oneFile.isDirectory()) 
+                {                                    
+                    addAllFromFolder(new File(oneFile.getAbsolutePath()));
+                }
+                else if(oneFile.getName().endsWith(".mp3"))
+                     {
+                        filezzHashMap.put(oneFile.getAbsolutePath(),oneFile.getName());
+                        mp3Number++;
+                     }
+            }
+        }
+        return mp3Number;
+    }
+
+    public Object getNumberOfFiles() {
+        return filezzHashMap.size();
+    }
+    
+    public List<String> getDuplicates()
+    {
+        final Collection<String> values = filezzHashMap.values();
+        List<String> tempTotallyBadWayList = new ArrayList<>();
+        for(String tempoValue : values)
+        {
+            tempTotallyBadWayList.add(tempoValue);
+        }
+            for(String duplicate : findDuplicates(tempTotallyBadWayList))
+            {
+                for (Map.Entry<String, String> entry : filezzHashMap.entrySet()) 
+                {
+                    if (duplicate.equals(entry.getValue())) {
+                        duplicatezzList.add(entry.getKey());                        
+                    }
+                }
+            }
+        return duplicatezzList;
+    }
+    
+      public static Set<String> findDuplicates(List<String> listContainingDuplicates) {
+ 
+		final Set<String> setToReturn = new HashSet<String>();
+		final Set<String> set1 = new HashSet<String>();
+ 
+		for (String yourInt : listContainingDuplicates) {
+			if (!set1.add(yourInt)) {
+				setToReturn.add(yourInt);
+			}
+		}
+		return setToReturn;
+	}    
+}
---
+++ test/TestFiles.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import src.Files;
+
+/**
+ *
+ * @author filipovsluha
+ */
+public class TestFiles {
+    
+    public TestFiles() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void TestAddingFiles() 
+     {
+        Files files = new Files();
+        files.add("path","fileName");
+        assertEquals(1, files.getNumberOfFiles());
+     }
+}
